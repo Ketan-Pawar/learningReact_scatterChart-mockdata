a1be49e67e49c886dab7fa8ea3b3223c4069d554
Ketan Pawar <ketanpawar@FVFGV626Q05N.local>
2022-06-07 11:17:09 +0530

    “deployed”

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 
 function App() {
+
+  const [data] = useState([
+    [90, 20],
+    [20, 100],
+    [66, 44],
+    [53, 80],
+    [24, 182],
+    [80, 72],
+    [10, 76],
+    [33, 150],
+    [100, 15],
+    [50, 50]
+  ]);
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+     // Setting up conatiner
+     const w = 400;
+     const h = 300;
+     const svg = d3.select(svgRef.current)
+      .attr('width', w)
+      .attr('height', h)
+      .style('overflow', 'visible')
+      .style('margin-top', '100px');
+
+    // Setting up scaling
+    const xScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, w]);
+    const yScale = d3.scaleLinear()
+      .domain([0, 200])
+      .range([h, 0]);
+
+    // Setting up axis
+    const xAxis = d3.axisBottom(xScale).ticks(data.length);
+    const yAxis = d3.axisLeft(yScale).ticks(10);
+    svg.append('g')
+      .call(xAxis)
+      .attr('transform', `translate(0, ${h})`);
+    svg.append('g')
+      .call(yAxis);
+
+    // Setting up axis labeling
+    svg.append('text')
+      .attr('x', w/2)
+      .attr('y', h + 50)
+      .text('x');
+    svg.append('text')
+      .attr('y', h/2)
+      .attr('x', -50)
+      .text('y');
+
+    // Setting up svg data
+    svg.selectAll()
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr('cx', d => xScale(d[0]))
+        .attr('cy', d => yScale(d[1]))
+        .attr('r', 2);
+  }, [data]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Scatter Chart</h1> */}
+      <svg ref={svgRef}></svg>
     </div>
   );
 }
